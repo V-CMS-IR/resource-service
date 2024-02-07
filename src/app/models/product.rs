a1f57d4c58f1b62aac11fa281073cb0881beed68
Hossein Salehi <ho.off.acc@gmail.com>
2024-02-07 06:48:12 +0330

    FEATURE | add get product and get products to fetch one and many products

--- src/app/models/product.rs
+++ src/app/models/product.rs
@@ -1,30 +1,32 @@
+use std::sync::{Arc, Mutex};
 use async_graphql::{Enum, Error, Object, Result, SimpleObject};
-use mongodb::bson::{DateTime, doc};
-use mongodb::Database;
-use rm_orm::preload::*;
+use mongodb::bson::doc;
+use rm_orm::{Model, ProxyModelCrud, RmORM};
+use rm_orm::model::Prototype;
 use serde::{Deserialize, Serialize};
 
-use crate::types::DateWrapper;
+use crate::types::{DateWrapper, ObjectID};
 
 //TODO Write A Comment For all section of it and separate Sections
 type Creator = i16;
 
 
-#[derive(Model, Default, SimpleObject, Debug, Serialize, Deserialize)]
-#[coll_name = "Products"]
+// #[derive(Model, Default, SimpleObject, Debug, Serialize, Deserialize)]
+// #[coll_name = "Products"]
+#[Model(coll_name = "Products")]
+#[derive(SimpleObject, Serialize, Deserialize, Default)]
 pub struct Product {
-    #[graphql(skip)]
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    id: Option<String>,
+    #[serde(skip_serializing_if = "ObjectID::is_none")]
+    _id: ObjectID,
     pub title: String,
     pub description: Option<String>,
     pub content: Option<Content>,
     pub status: Status,
     pub meta: Option<Vec<Meta>>,
     pub author: Creator,
-    pub created_at:  DateWrapper<DateTime>,
-    pub update_at: DateWrapper<DateTime>,
-    pub deleted_at: DateWrapper<DateTime>
+    pub created_at: DateWrapper,
+    pub updated_at: DateWrapper,
+    pub deleted_at: DateWrapper,
 }
 
 #[derive(SimpleObject, Debug, Serialize, Deserialize)]
@@ -69,8 +71,42 @@ pub struct ProductMutation;
 
 #[Object]
 impl ProductQuery {
-    async fn get_enums(&self, name: Status) -> Result<Status> {
-        Ok(name)
+    async fn get_product(&self, object_id: Option<ObjectID>) -> Result<Option<Product>, Error> {
+        let db = RmORM::get_db();
+        let mut product = Product::new_model(&db);
+        let mut sample = doc! {
+            "author": 0
+        };
+        if object_id.is_some() {
+            let ob = object_id.unwrap().0;
+            sample.insert("_id", ob);
+        }
+        let re = product.find_one(
+            Prototype::Doc(sample)
+        ).await?;
+
+        Ok(re)
+    }
+    async fn get_products(&self) -> Result<Vec<Product>, Error> {
+        let db = RmORM::get_db();
+        let product = Product::new_model(&db);
+        let sample = Prototype::Doc(
+            doc! {
+                "author": 0
+            }
+        );
+        let result = Arc::new(Mutex::new(Vec::new())); // Wrap result in an Arc and a Mutex
+         product.find_with_callback(sample, {
+            let result = Arc::clone(&result); // Clone Arc for the closure
+            move |pr| {
+                // Accessing result inside the closure
+                let mut result = result.lock().unwrap(); // Obtain the lock
+                result.push(pr); // Pushing a String into the vector
+            }
+        }).await;
+        let arc_mutex_inner = Arc::try_unwrap(result).map_err(|_| Error::new("Can't get the result"))?;
+        let mutex_inner = arc_mutex_inner.into_inner().map_err(|_| Error::new("Can't get the result"))?;
+        Ok(mutex_inner)
     }
 }
 
@@ -79,16 +115,36 @@ impl ProductMutation {
     async fn new_product<'a>(
         &self,
         title: String,
+        content: Option<String>,
         description: Option<String>,
         status: Option<Status>,
     ) -> Result<String, Error> {
         let db = RmORM::get_db();
-        let mut product = Product::new(&db).await;
+        let mut product = Product::new_model(&db);
         product.title = title;
         product.description = description;
         product.status = status.unwrap_or_default();
+        if content.is_some() {
+            product.content = Some(Content {
+                params: vec![],
+                raw_content: content.unwrap(),
+            });
+        }
         let re = product.save().await?;
         Ok(re.inserted_id.to_string())
+    }
+
+    async fn update_product(&self ,
+                            object_id: String,
+                            title: Option<String> ,
+                            content: Option<String>,
+                            description: Option<String>,
+                            status: Option<Status>,
+    ) -> Result<String , Error>{
+        let db = RmORM::get_db();
+        let mut product = Product::new_model(&db);
+        product._id = object_id.into();
+        Ok("".into())
     }
 }
 
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,13 +1,32 @@
 use async_graphql::scalar;
-use mongodb::bson::DateTime;
+use mongodb::bson::{Bson, DateTime};
+use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize , Deserialize , Debug)]
-pub struct DateWrapper<T>(T);
+pub struct DateWrapper(DateTime);
 
-impl Default for DateWrapper<DateTime>{
+impl Default for DateWrapper{
     fn default() -> Self {
         DateWrapper(DateTime::now())
     }
 }
-scalar!(DateWrapper<DateTime>);
+scalar!(DateWrapper);
+
+#[derive(Serialize , Deserialize , Debug , Default)]
+pub struct ObjectID(pub Option<ObjectId>);
+scalar!(ObjectID);
+
+impl From<String> for ObjectID{
+    fn from(value: String) -> Self {
+        ObjectID(
+            Bson::from(value).as_object_id()
+        )
+    }
+}
+impl ObjectID{
+    pub fn is_none(val : &ObjectID)->bool{
+        val.0.is_none()
+    }
+}
+
